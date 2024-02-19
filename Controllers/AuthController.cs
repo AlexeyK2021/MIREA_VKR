1ca0e86dd2603a4e69eccbad008adb9367a32041
Alexey Kalashnikov <kalashnickov.alexey2015@yandex.ru>
2024-02-19 16:57:17 +0300

    Started proj and added auth page

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace opcUaWebMVC.Controllers;
+
+public class AuthController : Controller
+{
+    private readonly ILogger<AuthController> _logger;
+    private string? loggedUsername = null;
+
+    public AuthController(ILogger<AuthController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpPost]
+    public IActionResult AuthPage(string login, string passwd)
+    {
+        loggedUsername = login;
+        Console.Write($"Login:{login}, passwd:{passwd}");
+        return Redirect("/Home/Index");
+    }
+
+    [HttpGet]
+    public IActionResult AuthPage()
+    {
+        return View(viewName: "AuthPage");
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using opcUaWebMVC.Models;
+
+namespace opcUaWebMVC.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        var names = new List<string>(){ "Tom", "Sam", "Bob" };
+        return View(names);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
